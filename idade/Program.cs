ead0f3171fda840ddabc69c45e0a3b961fbbde52
MatheusBernardi <etecpessoal2010@gmail.com>
2026-04-06 11:44:24 -0300

    meu primeiro commit

---
+++ idade/Program.cs
@@ -0,0 +1,8 @@
+﻿//declaração de variaveis
+string anoa = "2026";
+string ano = "";
+int idade = 0;
+
+Console.Write ("escreva o ano em que você nasceu: ");
+ano = (Console.ReadLine ());
+idade = (anoa - ano);
---
+++ projeto1/Program.cs
@@ -0,0 +1,11 @@
+﻿// conversor de medidas
+// declaração de variaveis
+double medida = 0;
+double valor =0;
+
+Console.Write("Digite o valor em milhas: ");
+valor = Convert.ToDouble (Console.ReadLine());
+
+medida = valor * 1.6;
+
+Console.Write("Sua medida em KM é de: "+medida);
